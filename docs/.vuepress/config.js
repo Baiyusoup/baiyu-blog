92b9aa35f10e5fb37db87d313a91f8fb56abc70c
Baiyusoup <1575727840@qq.com>
2021-05-11 11:22:10 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ const themeConfig = require('./config/theme/')
 module.exports = {
   title: "青菜白玉汤",
   description: `青菜白玉汤 博客`,
+  base: '/baiyu/',
   dest: 'public',
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
